e8507fd18fbd1d3b7c9905985be5037f34842b25
darksamus6 <darksamus6@gmail.com>
2017-11-14 03:06:02 +0000

    first commit

---
+++ javascripts/script.js
@@ -0,0 +1,9 @@
+  $(document).ready(function(){
+
+    $('.button-collapse').sideNav({
+    	draggable: true, // Choose whether you can drag to open on touch screens,
+    });
+    //$('.carousel').carousel();
+    $('.scrollspy').scrollSpy();
+
+  });
