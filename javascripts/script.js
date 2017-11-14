4fd9a94c96faed399c553859a212edb929a01b27
darksamus6 <darksamus6@gmail.com>
2017-11-14 04:20:47 +0000

    html formatting

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,9 +1,7 @@
-  $(document).ready(function(){
-
-    $('.button-collapse').sideNav({
-    	draggable: true, // Choose whether you can drag to open on touch screens,
-    });
-    //$('.carousel').carousel();
-    $('.scrollspy').scrollSpy();
-
+  $(document).ready(function() {
+  	$('.button-collapse').sideNav({
+  		draggable: true, // Choose whether you can drag to open on touch screens,
+  	});
+  	//$('.carousel').carousel();
+  	$('.scrollspy').scrollSpy();
   });
